a29a7351bc95a723be6f1e1a01032c6a90ad5dec
vanguard <vanguardd@outlook.com>
2020-05-09 23:09:12 +0800

    完成网关登录拦截

---
+++ leyou/ly-gateway/src/main/java/com/leyou/gateway/config/JwtProperties.java
@@ -0,0 +1,32 @@
+package com.leyou.gateway.config;
+
+import com.leyou.auth.utils.RsaUtils;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import javax.annotation.PostConstruct;
+import java.security.PublicKey;
+
+@Data
+@Slf4j
+@ConfigurationProperties(prefix = "leyou.jwt")
+public class JwtProperties {
+
+    private String pubKeyPath;// 公钥
+
+    private PublicKey publicKey; // 公钥
+
+    private String cookieName;
+
+    @PostConstruct
+    public void init(){
+        try {
+            // 获取公钥和私钥
+            this.publicKey = RsaUtils.getPublicKey(pubKeyPath);
+        } catch (Exception e) {
+            log.error("初始化公钥失败！", e);
+            throw new RuntimeException();
+        }
+    }
+}
---
+++ leyou/ly-gateway/src/main/java/com/leyou/gateway/filter/LoginFilter.java
@@ -0,0 +1,81 @@
+package com.leyou.gateway.filter;
+
+import com.leyou.auth.utils.JwtUtils;
+import com.leyou.common.utils.CookieUtils;
+import com.leyou.gateway.config.FilterProperties;
+import com.leyou.gateway.config.JwtProperties;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.http.HttpStatus;
+import org.apache.http.protocol.RequestContent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Title:
+ * @Description:
+ * @Author: vanguard
+ * @Version: 1.0
+ * @Date: 2020/05/09
+ */
+@Component
+@EnableConfigurationProperties({JwtProperties.class, FilterProperties.class})
+public class LoginFilter extends ZuulFilter {
+
+    @Autowired
+    private JwtProperties jwtProperties;
+
+    @Autowired
+    private FilterProperties filterProperties;
+
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 5;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        // 获取上下文
+        RequestContext ctx = RequestContext.getCurrentContext();
+        // 获取request
+        HttpServletRequest req = ctx.getRequest();
+        // 获取路径
+        String requestURI = req.getRequestURI();
+        // 判断白名单
+        // 遍历允许访问的路径
+        for (String path : filterProperties.getAllowPaths()) {
+            // 然后判断是否是符合
+            if(requestURI.startsWith(path)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        // 获取上下文
+        RequestContext context = RequestContext.getCurrentContext();
+        // 获取request
+        HttpServletRequest request = context.getRequest();
+        // 获取token
+        String token = CookieUtils.getCookieValue(request, jwtProperties.getCookieName());
+        try {
+            JwtUtils.getInfoFromToken(token, jwtProperties.getPublicKey());
+        } catch (Exception e) {
+            context.setSendZuulResponse(false);
+            context.setResponseStatusCode(HttpStatus.SC_FORBIDDEN);
+        }
+        return null;
+    }
+}
